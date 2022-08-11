59994d45dde04b02760da2720202dece005bebdb
DirtyHeroine <przemex17@wp.pl>
2022-08-11 10:18:05 +0200

    Working Beta

--- Main.js
+++ Main.js
@@ -1,16 +1,59 @@
 const WebSocket = require('ws');
+const fs = require('fs');
 const nodemailer = require('nodemailer');
-const { SMTP, port } = require('./config.json');
+const { chkDate } = require('./Date');
+const { log, buildHTML } = require('./Utils');
+const { SMTP, port, sender } = require('./config.json');
 const transport = nodemailer.createTransport(SMTP);
-const { getDate } = require('./Date');
-const { log } = require('./Utils');
+var emails = new Array();
+let toSend = "";
+
+Setup();
+Sender();
+
 const wss = new WebSocket.Server({ port: port });
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-    log('%s', message);
-    //Add to table
-    //table to buildhtml
-    //send html
+    log(message);
+    toSend = toSend + message + "<br>";
   });
 });
 
+async function Sending(){
+  for (j = 0; j < emails.length; j++) {
+    let message = {
+      from: sender,
+      to: emails[j],
+      subject: 'Monitoring Serwisów WWW',
+      html: buildHTML(toSend)
+    };
+    transport.sendMail(message, (err) => {
+      if (err) {
+        log("Błąd wysyłania >> " + message.to);
+        log(err.message);
+        return false;
+      } else {
+        log("Wysłano >> "+ message.to);
+        return true;
+      }  
+    });
+  }
+}
+
+async function Sender(){
+  if(chkDate()) Sending();
+  await new Promise(res=>setTimeout(res,60000));
+  process.nextTick(Sender);
+}
+
+function Setup(){
+  console.log("======START======");
+  console.log("==[NOTIFYKEEPS]==");
+  console.log("");
+  log("Ładowanie emaili:");
+  fs.readFileSync('./emails.txt', 'utf-8').split(/\r?\n/).forEach(function(line) { if (line != '') { emails.push(line); } });
+  console.log(emails);
+  console.log("=================");
+  log("Praca w tle rozpoczęta...");
+  log("Życzę miłego dnia i smacznej Kawusi :)")
+}
--- Utils.js
+++ Utils.js
@@ -3,15 +3,8 @@ function log(text) {
     let welcome = "[NotifyKeep] (" + getDate() + ") >> ";
     console.log(welcome + text);
 }
-function buildHTML(dane){
-    //pass table
-    var start = "<html><body><b>Monitoring serwisów WWW:</b><br>";
-    //loop table for add line
-
-    //add st00pke with date/time
-    var time = "<br><br>Dnia: " + "data" + "<br>" +
-        "Godzina: " + "godzina";
-
-    var end = "</body></html>";
+function buildHTML(data){
+    return "<html><body><b>Monitoring serwisów WWW:</b><br>" + data +
+    "<br><br>" + getDate() + "</body></html>";
 }
 module.exports = { log, buildHTML }
